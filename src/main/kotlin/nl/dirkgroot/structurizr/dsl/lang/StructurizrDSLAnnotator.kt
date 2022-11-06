8c18a709b927b56d49424937d9ac312fac0aca37
Dirk Groot <dirkgroot77@gmail.com>
2022-11-06 09:51:41 +0100

    Remove commented code

--- src/main/kotlin/nl/dirkgroot/structurizr/dsl/lang/StructurizrDSLAnnotator.kt
+++ src/main/kotlin/nl/dirkgroot/structurizr/dsl/lang/StructurizrDSLAnnotator.kt
@@ -6,19 +6,5 @@ import com.intellij.psi.PsiElement
 
 class StructurizrDSLAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-//        when (element) {
-//            is SDSoftwareSystemName ->
-//                holder.createTextAttribute(element, StructurizrDSLSyntaxHighlighter.SOFTWARE_SYSTEM_NAME)
-//
-//            is SDSoftwareSystemIdentifier ->
-//                holder.createTextAttribute(element, StructurizrDSLSyntaxHighlighter.IDENTIFIER)
-//        }
     }
-
-//    private fun AnnotationHolder.createTextAttribute(element: PsiElement, textAttributesKey: TextAttributesKey) {
-//        newSilentAnnotation(HighlightSeverity.INFORMATION)
-//            .range(element)
-//            .textAttributes(textAttributesKey)
-//            .create()
-//    }
 }
