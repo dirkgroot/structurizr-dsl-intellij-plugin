ebcbd632db03b9588df188d19a5cda621fa37330
Dirk Groot <dirkgroot77@gmail.com>
2022-12-05 21:21:47 +0100

    Update to latest IntelliJ platform, use JetBrains template as a basis

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
+import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+fun properties(key: String) = project.findProperty(key).toString()
+
 plugins {
-    kotlin("jvm") version "1.7.20"
-    java
-    id("org.jetbrains.intellij") version "1.9.0"
-    id("org.jetbrains.grammarkit") version "2021.2.1"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.changelog") version "2.0.0"
+    id("org.jetbrains.grammarkit") version "2022.3"
 }
 
 group = "nl.dirkgroot"
@@ -14,37 +18,57 @@ repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2022.2.3")
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
 }
 
 sourceSets["main"].java.srcDirs("src/main/gen")
 
 dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
 }
 
 tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+    wrapper {
+        gradleVersion = properties("gradleVersion")
+    }
+
+    test {
+        useJUnitPlatform()
     }
 
     withType<KotlinCompile> {
         dependsOn("generateLexer")
-
-        kotlinOptions {
-            jvmTarget = "11"
-        }
     }
 
     patchPluginXml {
-        changeNotes.set(
-            """
-            Add change notes here.<br>
-            <em>most HTML tags may be used</em>
-            """.trimIndent()
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            file("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").let { markdownToHTML(it) }
         )
     }
 
@@ -60,5 +84,20 @@ tasks {
 
         // if set, plugin will remove a lexer output file before generating new one. Default: false
         purgeOldFiles.set(true)
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        dependsOn("patchChangelog")
+        token.set(System.getenv("PUBLISH_TOKEN"))
+        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
 }
