bdca7e9e5399924d864393374c354d321695dbe2
Dirk Groot <dirkgroot77@gmail.com>
2023-03-30 14:14:28 +0200

    Remove unfinished parser
    Development on the parser will continue in a separate branch.

--- src/main/gen/nl/dirkgroot/structurizr/dsl/psi/SDRootElement.java
+++ src/main/gen/nl/dirkgroot/structurizr/dsl/psi/SDRootElement.java
@@ -5,12 +5,6 @@ import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
 
-public interface SDImplicitRelationship extends PsiElement {
-
-  @NotNull
-  List<SDArgument> getArgumentList();
-
-  @NotNull
-  SDIdentifierName getIdentifierName();
+public interface SDRootElement extends PsiElement {
 
 }
--- src/main/gen/nl/dirkgroot/structurizr/dsl/psi/impl/SDRootElementImpl.java
+++ src/main/gen/nl/dirkgroot/structurizr/dsl/psi/impl/SDRootElementImpl.java
@@ -11,32 +11,20 @@ import static nl.dirkgroot.structurizr.dsl.psi.SDTypes.*;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import nl.dirkgroot.structurizr.dsl.psi.*;
 
-public class SDImplicitRelationshipImpl extends ASTWrapperPsiElement implements SDImplicitRelationship {
+public class SDRootElementImpl extends ASTWrapperPsiElement implements SDRootElement {
 
-  public SDImplicitRelationshipImpl(@NotNull ASTNode node) {
+  public SDRootElementImpl(@NotNull ASTNode node) {
     super(node);
   }
 
   public void accept(@NotNull SDVisitor visitor) {
-    visitor.visitImplicitRelationship(this);
+    visitor.visitRootElement(this);
   }
 
   @Override
   public void accept(@NotNull PsiElementVisitor visitor) {
     if (visitor instanceof SDVisitor) accept((SDVisitor)visitor);
     else super.accept(visitor);
-  }
-
-  @Override
-  @NotNull
-  public List<SDArgument> getArgumentList() {
-    return PsiTreeUtil.getChildrenOfTypeAsList(this, SDArgument.class);
-  }
-
-  @Override
-  @NotNull
-  public SDIdentifierName getIdentifierName() {
-    return findNotNullChildByClass(SDIdentifierName.class);
   }
 
 }
