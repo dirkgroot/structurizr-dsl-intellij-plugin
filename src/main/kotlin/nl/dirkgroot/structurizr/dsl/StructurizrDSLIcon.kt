bd4217c55e62941e2302741315fd0dd258f58296
Dirk Groot <dirkgroot77@gmail.com>
2023-03-31 16:09:08 +0200

    Replace the Structurizr logo with a more generic one
    Refer to https://github.com/structurizr/dsl/discussions/240 for some
    background information about this change.

--- src/main/kotlin/nl/dirkgroot/structurizr/dsl/StructurizrDSLIcon.kt
+++ src/main/kotlin/nl/dirkgroot/structurizr/dsl/StructurizrDSLIcon.kt
@@ -3,5 +3,5 @@ package nl.dirkgroot.structurizr.dsl
 import com.intellij.openapi.util.IconLoader
 
 object StructurizrDSLIcon {
-    val icon = IconLoader.getIcon("/icons/structurizr.png", StructurizrDSLIcon::class.java)
+    val icon = IconLoader.getIcon("/fileIcon.svg", StructurizrDSLIcon::class.java)
 }
